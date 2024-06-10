554baf07ad679fcfd583c1b7a34a48dce4a8cba8
TheForeverOptimist <sean@elevate.app>
2024-06-10 14:36:32 -0700

    modified data files and updated router instances

--- api/src/lib/server.js
+++ api/src/lib/server.js
@@ -1,35 +1,12 @@
 import express from "express";
 import config from "./config.js";
+import { setupRoutes } from "./setupRoutes.js";
 
 export const startServer = () => {
   const httpServer = express();
   const port = config.port;
 
-  const router = express.Router();
-
-  router.use("/", (req, res, next) => {
-    console.log(`1 - router middleware function one`);
-    next();
-  });
-
-  httpServer.get("/api/ping", (req, res) => {
-    console.log(`🛠️ - Ping route: ${req.url} ${Date.now()}`);
-    res.status(200).json({
-      message: `✅ - Pong: test succesful`,
-    });
-  });
-
-  router.all("*", (req, res) => {
-    console.log(`2 - router middleware catch all`);
-    res.status(404).json({
-      message: `${req.url} not found`,
-    });
-  });
-
-  httpServer.use("/", router);
-
-  //TODO -- abstract this to the router
-  //TEST route for port config
+  setupRoutes(httpServer);
 
   try {
     httpServer.listen(port, () => {
--- api/src/lib/setupRoutes.js
+++ api/src/lib/setupRoutes.js
@@ -1,5 +1,16 @@
+import express from "express";
+
+const router = express.Router();
+
+export const setupRoutes = (app) => {
+  console.log(`\n Testing setup routes function...\n `);
+
+  router.use("/", (req, res) => {
+    console.log(`\n1 - Main router middleware function activated`);
+    res.sendStatus(200);
+  });
+
+  app.use('/api', router)
 
 
-export const setupRoutes() => {
-    console.log(`\n Testing setup routes function `)
-}
+};
